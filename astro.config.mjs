a7c32eff2a5eeab6bba3c21bf4936b9c015748ad
Rodolfo Fuentealba <rodfuentealbauno@gmail.com>
2026-07-30 14:51:09 -0400

    ✨ addSeoMetaTags

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://www.groundservice.cl",
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
